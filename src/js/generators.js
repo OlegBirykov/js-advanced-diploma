106ebd3157ad9aeab5206d995c62c50cbb46e65a
OlegBirykov <p_petro75@mail.ru>
2020-08-20 22:21:51 +0300

    Доработана генерация персонажей

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,19 +1,18 @@
 import PositionedCharacter from './PositionedCharacter';
 
-export function* characterGenerator(allowedTypes, maxLevel) {
-  for (const Type of allowedTypes) {
-    for (let level = 1; level <= maxLevel; level++) {
-      yield { Type, level };
-    }
+export function* characterGenerator(allowedTypes, maxLevel, characterCount) {
+  for (let i = 0; i < characterCount; i++) {
+    yield new allowedTypes[Math.trunc(Math.random() * allowedTypes.length)](
+      Math.trunc(Math.random() * maxLevel) + 1,
+    );
   }
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   const result = [];
-  const generator = [...characterGenerator(allowedTypes, maxLevel)];
-  for (let i = 0; i < characterCount; i++) {
-    const { Type, level } = generator[Math.trunc(Math.random() * generator.length)];
-    result.push(new PositionedCharacter(new Type(level), -1));
+  const generator = characterGenerator(allowedTypes, maxLevel, characterCount);
+  for (const character of generator) {
+    result.push(new PositionedCharacter(character, -1));
   }
   return result;
 }
